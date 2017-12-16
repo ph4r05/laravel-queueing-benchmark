27219b5ab713f98b371383e7c6013e9e8bf249c4
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-16 01:01:05 +0100

    worker - protocol verif

--- app/Benchmark/Queue/Ph4Worker.php
+++ app/Benchmark/Queue/Ph4Worker.php
@@ -8,6 +8,8 @@
 
 namespace App\Benchmark\Queue;
 
+use App\Benchmark\Utils;
+use App\Protocol;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Queue\QueueManager;
@@ -18,12 +20,19 @@ use Illuminate\Support\Facades\Log;
 
 class Ph4Worker extends Worker
 {
+    /**
+     * Verify correctness - protocol the run
+     * @var bool
+     */
+    protected $verify = false;
+
     public function __construct(QueueManager $manager, Dispatcher $events, ExceptionHandler $exceptions)
     {
         parent::__construct($manager, $events, $exceptions);
-        Log::info('Worker created');
-    }
 
+        $this->verify = Utils::bool(config('benchmark.verify_queueing'));
+        Log::info('Worker created ' . getmypid() . ', verify: ' . var_export($this->verify, true));
+    }
 
     /**
      * @param WorkerOptions $options
@@ -62,10 +71,19 @@ class Ph4Worker extends Worker
      */
     public function process($connectionName, $job, WorkerOptions $options)
     {
-        Log::info($job->id);
+        // Verify protocol.
+        // We could also capture \Events\JobProcessing
+        if ($this->verify && method_exists($job, 'getJobId')){
+            $proto = new Protocol([
+                'tstamp' => ceil(microtime(true) * 100000),
+                'pid' => getmypid(),
+                'jid' => $job->getJobId(),
+            ]);
+            $proto->save();
+        }
+
         return parent::process($connectionName, $job, $options);
     }
-
 
 }
 
