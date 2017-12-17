ed50272789b465e6ac6efff4d77a8d5b4fc59514
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-17 13:46:36 +0100

    bench script update

--- bench.sh
+++ bench.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Beanstalkd"
 php artisan app:feedJobs --batch --conn=2 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=0 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=2 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=1 --repeat 10 ;\
-echo "Optimistic: " \;
+echo "Redis" ;\
+php artisan app:feedJobs --batch --conn=3 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=0 --repeat 10 ;\
+php artisan app:feedJobs --batch --conn=3 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=1 --repeat 10 ;\
+echo "Optimistic: " ;\
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=0 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=1 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=1 --verify=0 --repeat 10 ;\
@@ -16,8 +20,8 @@ php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false 
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=9 --verify=0 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=9 --verify=1 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=10 --verify=0 --repeat 10 ;\
-php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=10 --verify=1 --repeat 10 \;
-echo "Pessimistic:" \;
+php artisan app:feedJobs --batch --conn=1 --del-mark=true --del-tsx-fetch=false --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=10 --verify=1 --repeat 10 ;\
+echo "Pessimistic:" ;\
 php artisan app:feedJobs --batch --conn=0 --del-mark=false --del-tsx-fetch=true --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=0 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=0 --del-mark=false --del-tsx-fetch=true --del-tsx-retry=1 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=1 --repeat 10 ;\
 php artisan app:feedJobs --batch --conn=0 --del-mark=false --del-tsx-fetch=true --del-tsx-retry=0 --work-clone=0 --batch-size=10000 --window-strategy=0 --verify=0 --repeat 10 ;\
